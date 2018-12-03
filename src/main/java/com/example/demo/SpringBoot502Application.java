ae2b0d0bbbc2b032ee65e5e167fe8f5a55ecf43b
Jaelyn Moses <jaelynmoses@Jaelyns-MacBook-Pro-272.local>
2018-12-03 13:09:14 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot502Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot502Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot502Application.class, args);
+	}
+}
